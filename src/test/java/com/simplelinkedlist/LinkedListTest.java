fdaa8b0001dbe69705b1660a4cf70c8ada52b598
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-12 23:57:28 +0530

    Create a simple linked list

--- src/test/java/com/simplelinkedlist/LinkedListTest.java
+++ src/test/java/com/simplelinkedlist/LinkedListTest.java
@@ -1,4 +1,18 @@
 package com.simplelinkedlist;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+/* @Description- create a simple linked list of 50, 30, 70 .*/
+
 public class LinkedListTest {
+    @Test
+    public void whenPassThreeNumber_LinkedListShouldPassedTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(50);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
